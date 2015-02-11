07b80b54df134c7ffa31c897f0e2714b684c7872
Marcin Molenda <spamm@molenda.eu>
2015-02-11 22:47:35 +0100

    Updating .gitignore

--- missal1962/missal.py
+++ missal1962/missal.py
@@ -1,11 +1,7 @@
-import datetime
+from datetime import date, datetime, timedelta
 from dateutil.easter import easter
 import sys
 from constants import *
-
-
-# def date_to_str(date_):
-#     return date_.strftime("%Y-%m-%d")
 
 
 def get_index_of_date(empty_year, date_):
@@ -16,11 +12,11 @@ def get_index_of_date(empty_year, date_):
 
 def build_empty_year(year):
     empty_year = list()
-    day = datetime.date(year, 1, 1)
+    day = date(year, 1, 1)
 
     while day.year == year:
         empty_year.append([day, []])
-        day += datetime.timedelta(days=1)
+        day += timedelta(days=1)
 
     return empty_year
 
@@ -35,10 +31,10 @@ def calc_varday__dom_sanctae_familiae(year):
     """ Dominica Sanctae Familiae Jesu Mariae Joseph
     First Sunday after Epiphany (06 January)
     """
-    epiphany = datetime.date(year, 1, 6)
+    epiphany = date(year, 1, 6)
     wd = epiphany.weekday() 
     delta = 6 - wd if wd < 6 else 7
-    return epiphany + datetime.timedelta(days=delta)
+    return epiphany + timedelta(days=delta)
 
 
 def calc_varday__dom_septuagesima(year):
@@ -46,42 +42,43 @@ def calc_varday__dom_septuagesima(year):
     First day of a Ressurection Sunday related block.
     It's 63 days before Ressurection.
     """
-    return calc_varday__dom_ressurectionis(year) - datetime.timedelta(days=63)
+    return calc_varday__dom_ressurectionis(year) - timedelta(days=63)
 
 
 def calc_varday__dom_adventus(year):
     """ Dominica I Adventus
     Nov 27 (if it's Sunday) or closest Sunday
     """
-    advent = datetime.date(year, 11, 27)
+    advent = date(year, 11, 27)
     wd = advent.weekday()
     if wd != 6:
-        advent += datetime.timedelta(days=6-wd)
+        advent += timedelta(days=6-wd)
     return advent
 
 
 if __name__ == '__main__':
-    year = int(sys.argv[1])
+    year = int(sys.argv[1]) if len(sys.argv) > 1 else datetime.now().year
     empty_year = build_empty_year(year)
 
     blocks = (
-        (calc_varday__dom_sanctae_familiae, vardays__post_epiphania), # Post Epiphany
-        (calc_varday__dom_septuagesima, vardays__pascha), # Pascha related days
-        (calc_varday__dom_adventus, vardays__advent) # Advent
+        # Post Epiphany
+        (calc_varday__dom_sanctae_familiae, vardays__post_epiphania),
+        # Pascha related days
+        (calc_varday__dom_septuagesima, vardays__pascha),
+        # Advent
+        (calc_varday__dom_adventus, vardays__advent)
     )
 
     for calc_varday_func, dataset in blocks:
         index = get_index_of_date(empty_year, calc_varday_func(year))
         for ii, day in enumerate(dataset):
             empty_year[index + ii][1] = [day]
-    
 
     # Fixed days
-    # for date_, contents in empty_year:
-    #     date_id = date_.strftime("%m-%d")
-    #     days = list(set([ii for ii in fixdays if ii.startswith(date_id)]))
-    #     contents.extend(days)
+    for date_, contents in empty_year:
+        date_id = date_.strftime("%m-%d")
+        days = list(set([ii for ii in fixdays if ii.startswith(date_id)]))
+        contents.extend(days)
 
     for ii in empty_year:
         print ii[0].strftime('%A'), ii
-
