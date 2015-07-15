87bca39ecb09bc27e690f88624c737b4e086c958
Marcin Molenda <mmolenda@isightpartners.com>
2015-07-16 00:01:03 +0200

    Fixing fixed days' ranks

--- missal1962/rules.py
+++ missal1962/rules.py
@@ -26,5 +26,61 @@ b) dni Adwentu aż do dnia 16 grudnia włącznie, nie wymienione wyżej; ustępu
 Te dni, jeżeli napotkają przeszkodę, muszą być wspomniane.
 
 26. Wszystkie dni niewymienione w numerach 23-25 są dniami powszednimi IV klasy; jeżeli napotykają przeszkodę, nigdy się ich nie wspomina.
+
+---
+30. Wigiliami I klasy są:
+a) Wigilia Bożego Narodzenia, w razie zbiegu zajmuje ona miejsce 4 niedzieli Adwentu, której wobec tego nie wspomina się.
+b) Wigilia Zesłania Ducha Świętego
+
+Te wigilie mają pierwszeństwo przed wszystkimi świętami i nie dopuszczają żadnych wspomnień.
+
+---
+31. Wigiliami II klasy są:
+a) wigilia Wniebowstąpienia Pańskiego
+b) wigilia Wniebowzięcia NMP
+c) wigilia Narodzenia św. Jana Chrzciciela
+d) wigilia św. Apostołów Piotra i Pawła
+
+Te wigilie mają pierwszeństwo przed dniami liturgicznymi III i IV klasy i jeżeli
+napotykają przeszkodę wspomina się je zgodnie z zasadami rubryk.
+
+---
+32. Wigilią III klasy jest wigilia św. Wawrzyńca
+Ta wigilia ma pierwszeństwo przed dniami liturgicznymi IV klasy i jeżeli
+napotka przeszkodę, wspomina się ją zgodnie z zasadami rubryk.
+
+---
+33. Wigilię II lub III klasy zupełnie opuszcza się, jeżeli wypadnie w jakąkolwiek niedzielę lub święto 1 klasy,
+albo jeżeli święto, które poprzedza zostaje przeniesione lub zredukowane do wspomnienia.
+
+---
+
+67. Oktawą 2 klasy jest oktawa Bożego Narodzenia. Dni wewnątrz tej oktawy są 2 klasy; natomiast dzień ósmy jest 1 klasy.
+
+68. Oktawa Bożego Narodzenia jest zbudowana w szczególny sposób, a mianowicie:
+a) dnia 26 grudnia obchodzi się święto św. Stefana Pierwszego Męczennika (2 klasy);
+b) dnia 27 grudnia obchodzi się święto św. Jana Apostoła i Ewangelisty (2 klasy);
+c) dnia 28 grudnia obchodzi się święto Świętych Młodzianków Męczenników (2 klasy);
+d) dnia 29 grudnia obchodzi się wspomnienie św. Tomasza Biskupa i Męczennika;
+e) dnia 31 grudnia obchodzi się wspomnienie św. Sylwestra I Papieża i Wyznawcy;
+f) ze świąt partykularnych dopuszcza się tylko święta 1 klasy na cześć Świętych, którzy
+w tych dniach wpisani są w kalendarzu powszechnym, chociaż tylko jako wspomnienie, inne
+przenosi się na czas po oktawie.
+
+69. O niedzieli w oktawie Bożego Narodzenia, która przypada między 26 a 31 grudnia
+zawsze odprawia się Oficjum ze wspomnieniem święta przypadkiem zbiegającego się, chyba
+ że w niedzielę przypadnie święto 1 klasy; w takim wypadku obchodzi się święto ze wspomnieniem niedzieli.
+
+---
+
+w latach przestepnych luty ma 29 dni, sw macieja i nastepne przenosi sie o dzien pozniej
+
+---
+
+piątek po 1 niedzieli męki Panskiej - siedmiu bolesci nmp
+
+---
+
+
 """
 
