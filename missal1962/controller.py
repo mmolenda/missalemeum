d7623b9c5032d179201ee8643bd02b2c5b61040a
Marcin Molenda <spamm@molenda.eu>
2018-11-21 23:46:25 +0100

    Adding lru_cache to controller functions

--- missal1962/controller.py
+++ missal1962/controller.py
@@ -1,6 +1,8 @@
 import datetime
 import importlib
 from collections import namedtuple
+
+from functools import lru_cache
 from typing import Iterator, List, Tuple
 
 from kalendar.factory import MissalFactory
@@ -9,20 +11,17 @@ from propers.models import Proper
 from propers.parser import ProperParser
 
 
-missals = {}
-
-
+@lru_cache(maxsize=32)
 def get_calendar(year: int, lang) -> Calendar:
-    if year not in missals:
-        missal = MissalFactory.create(year, lang)
-        missals[year] = missal
-    return missals[year]
+    return MissalFactory.create(year, lang)
 
 
+@lru_cache(maxsize=32)
 def get_proper_by_id(proper_id: str, lang: str) -> Tuple[Proper, Proper]:
     return ProperParser.parse(proper_id.lower().replace('__', ':'), lang)
 
 
+@lru_cache(maxsize=32)
 def get_proper_by_date(date_: datetime.date, lang) -> List[Tuple[Proper, Proper]]:
     missal: Calendar = get_calendar(date_.year, lang)
     day: Day = missal.get_day(date_)
