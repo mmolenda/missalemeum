a55bcb0c09b4485a671768d1303a3f6dbc624404
Marcin Molenda <spamm@molenda.eu>
2018-11-21 23:55:37 +0100

    UI static structure

--- missal1962/controller.py
+++ missal1962/controller.py
@@ -1,6 +1,4 @@
 import datetime
-import importlib
-from collections import namedtuple
 
 from functools import lru_cache
 from typing import Iterator, List, Tuple
@@ -26,11 +24,3 @@ def get_proper_by_date(date_: datetime.date, lang) -> List[Tuple[Proper, Proper]
     missal: Calendar = get_calendar(date_.year, lang)
     day: Day = missal.get_day(date_)
     return day.get_proper()
-
-
-def search(search_string: str, lang: str) -> Iterator[namedtuple]:
-    titles = importlib.import_module(f'constants.{lang}.translation')
-    Result = namedtuple('Result', 'id title')
-    for id_, title in titles.titles.items():
-        if search_string.strip().lower() in title.lower():
-            yield Result(id=':'.join(id_.split(':')[:2]), title=title)
