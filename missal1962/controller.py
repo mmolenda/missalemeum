fe3086954a23f20ff7ea0d867e6cb1a8249fc9e7
Marcin Molenda <spamm@molenda.eu>
2018-12-08 08:07:52 +0100

    Reverting cache on date endpoint

--- missal1962/controller.py
+++ missal1962/controller.py
@@ -18,12 +18,10 @@ def get_calendar(year: int, lang) -> Calendar:
     return MissalFactory.create(year, lang)
 
 
-@lru_cache(maxsize=0 if no_cache else 256)
 def get_proper_by_id(proper_id: str, lang: str) -> Tuple[Proper, Proper]:
     return ProperParser.parse(proper_id.lower().replace('__', ':'), lang)
 
 
-@lru_cache(maxsize=0 if no_cache else 256)
 def get_proper_by_date(date_: datetime.date, lang) -> List[Tuple[Proper, Proper]]:
     missal: Calendar = get_calendar(date_.year, lang)
     day: Day = missal.get_day(date_)
