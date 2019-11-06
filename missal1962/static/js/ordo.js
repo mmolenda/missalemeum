9ba37ae47f2e533b8591f4ec0a50f3e3ed9ff9b8
Marcin Molenda <spamm@molenda.eu>
2019-11-07 00:54:42 +0100

    Ordo: activating menu items on scrolling

--- missal1962/static/js/ordo.js
+++ missal1962/static/js/ordo.js
@@ -1,11 +1,15 @@
 $(window).on("load", function () {
 
     const templateSectionTitle = $("#template-section-title").text();
+    const templateOrdoItem = $("#template-ordo-item").text();
     const templateSectionSubTitle = $("#template-section-subtitle").text();
     const templateRubric = $("#template-rubric").text();
     const templateContentColumns = $("#template-content-columns").text();
     const templateSidebarItem = $("#template-sidebar-ordo-item").text();
     const templateContentPrint = $("#template-content-print").text();
+
+    const $sidebar = $("nav#sidebar");
+    let scrollTimer;
 
     loadOrdo();
 
@@ -19,19 +23,25 @@ $(window).on("load", function () {
             window.scrollTo(0, 0);
             $(renderTemplate(templateSectionTitle, {})).appendTo($main);
             $.each(data, function(ii, item) {
-                $(renderTemplate(templateSidebarItem, {id: ii, title: item.title})).appendTo(sidebarUl);
-                $(renderTemplate(templateSectionSubTitle, {id: ii, title: item.title})).appendTo($main);
+                let $sidebarItem = $(renderTemplate(templateSidebarItem, {id: ii, title: item.title}));
+                if (ii === 0) {
+                    $sidebarItem.addClass("active");
+                }
+                $sidebarItem.appendTo(sidebarUl);
+                let $ordoItem = $(renderTemplate(templateOrdoItem, {id: ii}));
+                $(renderTemplate(templateSectionSubTitle, {title: item.title})).appendTo($ordoItem);
                 $.each(item.body, function(jj, bodyItem) {
                     if (typeof bodyItem == "string") {
-                        $(renderTemplate(templateRubric, {rubric: bodyItem.split("\n").join("<br>")})).appendTo($main);
+                        $(renderTemplate(templateRubric, {rubric: bodyItem.split("\n").join("<br>")})).appendTo($ordoItem);
                     } else {
                         $.each(bodyItem, function(x, y) {bodyItem[x] = y.replace(/\*([^\*]+)\*/g, "<em>$1</em>")})
                         $(renderTemplate(templateContentColumns, {
                             sectionVernacular: bodyItem[0].split("\n").join("<br>"),
                             sectionLatin: bodyItem[1].split("\n").join("<br>")
-                        })).appendTo($main);
+                        })).appendTo($ordoItem);
                     }
                 });
+                $ordoItem.appendTo($main);
             });
             adaptSectionColumns();
             hideLoader();
@@ -56,6 +66,32 @@ $(window).on("load", function () {
         }
         // need to wait a bit until the columns are resized back after closing the sidebar
         setTimeout(function() {$(window).scrollTop(ordoItem.offset().top - 70);}, 350);
+    });
+
+    $(window).scroll(function() {
+        /**
+         * Highlight displayed section in the menu
+         **/
+        if(scrollTimer) {
+            window.clearTimeout(scrollTimer);
+        }
+
+        scrollTimer = window.setTimeout(function() {
+            let windowPosition = $(this).scrollTop();
+            let scrolledSections = $.grep($('.ordo-item'), function (v) {
+                return windowPosition >= $(v).offset().top - 150;
+            });
+            let displayedSection = $(scrolledSections).last();
+            let displayedSectionId = displayedSection.attr('id');
+            $("#sidebar li.sidebar-ordo-item").removeClass("active");
+            let sidebarItem = $("#sidebar-" + displayedSectionId);
+            sidebarItem.addClass("active");
+            let sidebarItemPosition = sidebarItem.position().top;
+            let sidebarPosition = Math.abs($sidebar.find("ul").position().top);
+            if ((sidebarItemPosition > $sidebar.height() * 0.6) || sidebarItemPosition < 0) {
+                $sidebar.animate({scrollTop: sidebarPosition + sidebarItemPosition - 100}, 200);
+            }
+        }, 100);
     });
 
     $("#print").on("click", function () {
