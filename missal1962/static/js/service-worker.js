6fb16a28de91984b2c795aeefa23f67453ca095d
Marcin Molenda <spamm@molenda.eu>
2019-11-19 12:13:02 +0100

    Caching static pages in service worker

--- missal1962/static/js/service-worker.js
+++ missal1962/static/js/service-worker.js
@@ -14,17 +14,22 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v2';
+const PRECACHE = 'precache-v3';
 const RUNTIME = 'runtime';
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
     '/',
+    '/ordo',
+    '/icalendar',
+    '/info',
     '/static/css/styles.css',
     '/static/js/common.js',
     '/static/js/conf.js',
     '/static/js/ordo.js',
     '/static/js/proprium.js',
-    '/static/img/loader.gif'
+    '/static/img/loader.gif',
+    '/static/img/icalendar1.png',
+    '/static/img/icalendar2.png'
 ];
 
 // The install handler takes care of precaching the resources we always need.
@@ -59,7 +64,6 @@ self.addEventListener('fetch', event => {
     if (event.request.url.startsWith(self.location.origin)) {
         event.respondWith(
             caches.match(event.request).then(cachedResponse => {
-                console.log(caches, event.request);
                 if (cachedResponse) {
                     return cachedResponse;
                 }
