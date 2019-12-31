2b9a8dcb542fc49e07bf153a2ffb89e69ef92968
ofrades <mig.silva@gmail.com>
2019-12-31 10:28:36 +0000

    pt to endpoint

--- missal1962/static/js/pt/conf.js
+++ missal1962/static/js/pt/conf.js
@@ -2,8 +2,8 @@
  * Configuration for data provided by the API
  **/
 let config = {
-  calendarEndpoint: "/api/v2/calendar/",
-  dateEndpoint: "/api/v2/date/",
+  calendarEndpoint: "/pt/api/v2/calendar/",
+  dateEndpoint: "/pt/api/v2/date/",
   canticumEndpoint: "/api/v2/supplement/canticum/",
   minDate: "1900-01-01",
   maxDate: "2100-12-31"
