18611b2f003f1537628bec9da04cf211472e0e82
Marcin Molenda <spamm@molenda.eu>
2020-03-17 00:08:11 +0100

    Fixing service worker

--- missal1962/static/js/service-worker.js
+++ missal1962/static/js/service-worker.js
@@ -14,7 +14,7 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v3.3.2';
+const PRECACHE = 'precache-v3.3.3';
 const RUNTIME = 'runtime';
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
@@ -33,7 +33,7 @@ const PRECACHE_URLS = [
     '/static/js/pl/conf.js',
     '/static/js/en/conf.js',
     '/static/js/proprium.js',
-    '/static/img/loader.gif',
+    '/static/img/loader.gif'
 ];
 
 // The install handler takes care of precaching the resources we always need.
