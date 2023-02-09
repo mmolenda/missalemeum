28878e335041d0274f84c8f36b6f371cb01cfb98
mmolenda <spamm@molenda.eu>
2023-02-09 14:32:24 +0100

    registering service worker

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -15,3 +15,6 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js');
+}
