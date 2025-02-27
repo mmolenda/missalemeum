723b2d131562a3a8580515dd98ba2c6d8f184a92
Marcin Molenda <spamm@molenda.eu>
2025-02-27 16:28:49 +0100

    handling 404

--- ui/app/[locale]/(root_layout)/page.tsx
+++ ui/app/[locale]/(root_layout)/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ListProper from "@/components/ListProper";
 import moment from "moment";
+import {notFound} from "next/navigation";
 
 export default async function Page({params}) {
   const locale = (await params).locale
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/${locale}/api/v5/calendar`, {mode: "cors"});
+  response.status !== 200 && notFound()
   const items = await response.json();
   return <ListProper lang={locale} year={moment().format("YYYY")} items={items}/>
 
