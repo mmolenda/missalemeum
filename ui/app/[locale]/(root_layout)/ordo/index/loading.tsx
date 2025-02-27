ccdd91cbefdbfe035ca7ef35bee1c38c8744fa69
Marcin Molenda <spamm@molenda.eu>
2025-02-27 10:41:45 +0100

    adding loading skeletons

---
+++ ui/app/[locale]/(root_layout)/ordo/index/loading.tsx
@@ -0,0 +1,8 @@
+"use client"
+
+import React from "react";
+import SkeletonIndex from "@/components/SkeletonIndex";
+
+export default function Loading() {
+  return <SkeletonIndex />
+}
---
+++ ui/app/[locale]/(root_layout)/supplement/[id]/loading.tsx
@@ -0,0 +1,8 @@
+"use client"
+
+import React from "react";
+import SkeletonContent from "@/components/SkeletonContent";
+
+export default function Loading() {
+  return <SkeletonContent />
+}
