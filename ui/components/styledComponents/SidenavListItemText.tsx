70512e5211c7d6e5b07397ff4bf1c7f6e99aae61
Marcin Molenda <spamm@molenda.eu>
2025-03-03 12:27:54 +0100

    Refactoring: types in SidenavListItemText

--- ui/components/styledComponents/SidenavListItemText.tsx
+++ ui/components/styledComponents/SidenavListItemText.tsx
@@ -3,18 +3,22 @@
 import React from 'react';
 import ListItemText from "@mui/material/ListItemText";
 
-export default function SidenavListItemText({prim, sec, rank = 0}: {prim: string, sec?: string, rank?: number }) {
-	let rankStyles = {
-		textTransform: [1, 2].includes(rank) ? "uppercase" : "none",
-		fontStyle: rank === 4 ? "italic" : "normal"
-	}
-
-	return <ListItemText
-		primary={prim}
-		secondary={sec}
-		slotProps={{
-			primary: {sx: {...{overflow: "hidden", whiteSpace: "nowrap", textOverflow: "ellipsis", fontFamily: (theme) => theme.typography.fontFamily}, ...rankStyles}},
-			secondary: {sx:  {overflow: "hidden", whiteSpace: "nowrap", textOverflow: "ellipsis", color: "secondary.main"}}
-		}}
-	/>
+export default function SidenavListItemText({prim, sec, rank = 0}: { prim: string, sec?: string, rank?: number }) {
+  return <ListItemText
+    primary={prim}
+    secondary={sec}
+    slotProps={{
+      primary: {
+        sx: {
+          overflow: "hidden",
+          whiteSpace: "nowrap",
+          textOverflow: "ellipsis",
+          textTransform: [1, 2].includes(rank) ? "uppercase" : "none",
+          fontStyle: rank === 4 ? "italic" : "normal",
+          fontFamily: (theme) => theme.typography.fontFamily
+        }
+      },
+      secondary: {sx: {overflow: "hidden", whiteSpace: "nowrap", textOverflow: "ellipsis", color: "secondary.main"}}
+    }}
+  />
 }
