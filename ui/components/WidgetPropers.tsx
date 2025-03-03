3bcbabcf08bb79bf4fdfa2a807326dcc67d5f92e
Marcin Molenda <spamm@molenda.eu>
2025-03-03 13:04:55 +0100

    Refactoring: types in WidgetPropers.tsx

--- ui/components/WidgetPropers.tsx
+++ ui/components/WidgetPropers.tsx
@@ -1,13 +1,13 @@
 "use client"
 
-import React, {useState} from "react";
+import React from "react";
 import {Box, Button, IconButton, Typography} from "@mui/material";
 import moment from "moment";
 import {appbarDarkGrey, yellowish} from "@/components/designTokens";
 import Logo from "./Logo";
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
-import {POWERED_BY, TODAY} from "./intl";
+import {Locale, POWERED_BY, TODAY} from "./intl";
 import {ContainerMedium} from "./styledComponents/ContainerMedium";
 import Link from "next/link";
 import {Link as MUILink} from "@mui/material";
@@ -24,17 +24,17 @@ export default function WidgetPropers({lang, id, contents, version}: {
 
   return (
     <><ContainerMedium sx={{mt: "0.5rem", textAlign: "right"}}>
-      <IconButton component={Link} size="small" variant="outlined"
+      <IconButton component={Link} size="small"
                   href={`/${lang}/widgets/propers/${date.subtract(1, 'days').format('YYYY-MM-DD')}`}><ArrowBackIcon/></IconButton>&nbsp;
-      <IconButton component={Link} size="small" variant="outlined"
-                  href={`/${lang}/widgets/propers/${date.add(2, 'days').format('YYYY-MM-DD')}`}><ArrowForwardIcon/></IconButton>
       <Button component={Link} size="small" variant="outlined"
               href={`/${lang}/widgets/propers/${moment().format('YYYY-MM-DD')}`}>{TODAY[lang as Locale]}</Button>&nbsp;
+      <IconButton component={Link} size="small"
+                  href={`/${lang}/widgets/propers/${date.add(2, 'days').format('YYYY-MM-DD')}`}><ArrowForwardIcon/></IconButton>
     </ContainerMedium>
       <BilingualContent
         lang={lang}
         id={id}
-        content={contents}
+        contents={contents}
         widgetMode={true}
       />
       {version && <ContainerMedium sx={{display: "flex", justifyContent: "space-between"}}>
