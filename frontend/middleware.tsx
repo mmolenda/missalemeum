e1d56f1ea1882a1a99952e70d76653fa31cd9db2
Marcin Molenda <spamm@molenda.eu>
2025-03-06 16:22:33 +0100

    changing accepted lang header parser

--- frontend/middleware.tsx
+++ frontend/middleware.tsx
@@ -1,16 +1,19 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 import { headers } from 'next/headers';
-import * as acceptLanguageParser from 'accept-language-parser';
+import { resolveAcceptLanguage } from 'resolve-accept-language'
+
 
 export async function middleware(request: NextRequest) {
-  let defaultLocale = "en"
+  let defaultLocale = "en-US"
   const headersList = await headers()
-  const acceptLanguage = headersList.get('accept-language') || defaultLocale
-  const languages = acceptLanguageParser.parse(acceptLanguage)
-  const primaryLanguage = languages.length > 0 ? languages[0].code : defaultLocale
-  const locale = primaryLanguage == "pl" ? "pl" : "en"
-  return NextResponse.redirect(new URL(`/${locale}`, request.url))
+  const acceptLanguageHeader = headersList.get('accept-language') || defaultLocale
+  const locale = resolveAcceptLanguage(
+    acceptLanguageHeader,
+    [defaultLocale, 'pl-PL'] as const,
+    defaultLocale
+  )
+  return NextResponse.redirect(new URL(`/${locale.split("-")[0]}`, request.url))
 }
 
 export const config = {
