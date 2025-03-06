ff97dec5e84d5f4d7f57942745cd0020fc82e27c
Marcin Molenda <spamm@molenda.eu>
2025-03-06 16:02:44 +0100

    Handling preferred locale

--- frontend/middleware.tsx
+++ frontend/middleware.tsx
@@ -1,10 +1,16 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+import { headers } from 'next/headers';
+import * as acceptLanguageParser from 'accept-language-parser';
 
-// This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   let defaultLocale = "en"
-  return NextResponse.redirect(new URL(`/${defaultLocale}`, request.url))
+  const headersList = await headers()
+  const acceptLanguage = headersList.get('accept-language') || defaultLocale
+  const languages = acceptLanguageParser.parse(acceptLanguage)
+  const primaryLanguage = languages.length > 0 ? languages[0].code : defaultLocale
+  const locale = primaryLanguage == "pl" ? "pl" : "en"
+  return NextResponse.redirect(new URL(`/${locale}`, request.url))
 }
 
 export const config = {
