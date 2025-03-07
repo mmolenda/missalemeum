64e47eade8d96b170db49435d584062b54bc0fe8
Marcin Molenda <spamm@molenda.eu>
2025-03-07 12:47:20 +0100

    Fixing lint error in MyLink

--- frontend/components/MyLink.tsx
+++ frontend/components/MyLink.tsx
@@ -8,6 +8,6 @@ export default function MyLink({href, text, widgetMode}: {href: string | undefin
 		? <MUILink target="_blank" href={(search && !search.startsWith("ref")) ? href : pathname}>{text}</MUILink>
 		: <MUILink
 			component={Link}
-			href={href}>{text}</MUILink>
+			href={href ?? "/"}>{text}</MUILink>
 }
 
