e447ce0a26d35908c6ab7e850681adc77b72c620
mmolenda <spamm@molenda.eu>
2025-08-18 12:54:04 +0200

    local mapping for Sancti/01-21

--- backend/tests/util.py
+++ backend/tests/util.py
@@ -100,10 +100,10 @@ if __name__ == "__main__":
     # for l in ['la', 'pl', 'en']:
         # generate_propers_fixtures(dates, l)
 
-    lang = "en"
+    lang = "pl"
     
     update_propers_for_dates(
-        [datetime.date(2024, 1, 20), datetime.date(2025, 1, 20)],
+        [datetime.date(2024, 1, 21), datetime.date(2025, 1, 21)],
         lang,
         os.path.join(HERE, "fixtures", f"propers_{lang}.json")
     )
