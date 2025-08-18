f40a2a968cb7d45396311be28b5a978d55710ad4
mmolenda <spamm@molenda.eu>
2025-08-18 16:42:52 +0200

    ocal mapping for Sancti/01-27

--- backend/tests/util.py
+++ backend/tests/util.py
@@ -103,7 +103,9 @@ if __name__ == "__main__":
     lang = "en"
     
     update_propers_for_dates(
-        [datetime.date(2024, 1, 26), datetime.date(2025, 1, 26)],
+        [datetime.date(2024, 1, 27),
+         datetime.date(2025, 1, 27)
+        ],
         lang,
         os.path.join(HERE, "fixtures", f"propers_{lang}.json")
     )
