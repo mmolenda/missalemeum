86cc73e338be2e8345b2584a2bed6ae8b0cc6115
mmolenda <spamm@molenda.eu>
2025-08-20 12:33:21 +0200

    local mapping for Tempora/Quad1-0

--- backend/tests/test_propers_local.py
+++ backend/tests/test_propers_local.py
@@ -16,11 +16,11 @@ def _get_proper_fixtures(fixture):
     # 01-25
 
     years = [
-        '2024',
+        # '2024',
         '2025',
     ]
     days = [
-        '02-17'
+        '02-18'
     ]
     dates = [f"{y}-{d}" for y in years for d in days]
     with open(os.path.join(HERE, 'fixtures/{}'.format(fixture))) as fh:
