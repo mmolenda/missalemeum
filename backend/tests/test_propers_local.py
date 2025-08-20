195ee080c9874afa38f7017c8430bf599aa12c53
mmolenda <spamm@molenda.eu>
2025-08-20 15:01:20 +0200

    local mapping for Sancti/02-23

--- backend/tests/test_propers_local.py
+++ backend/tests/test_propers_local.py
@@ -9,13 +9,13 @@ from .util import update_propers_for_dates
 from .conftest import get_missal, HERE
 
 languages = [
-    'pl',
+    # 'pl',
     'en',
     # 'la'
     ]
 years = [
-    '2024',
-    # '2025',
+    # '2024',
+    '2025',
 ]
 days = ['02-23']
 
