ae49bad5f40365f4729d2f677c27f139bedb255a
mmolenda <spamm@molenda.eu>
2025-08-26 01:00:43 +0200

    Fixed commemoration in 03-11

--- backend/tests/test_propers_local.py
+++ backend/tests/test_propers_local.py
@@ -21,8 +21,8 @@ days = [
     # '03-07',
     # '03-08',
     # '03-09',
-    '03-10',
-    # '03-11',
+    # '03-10',
+    '03-11',
     ]
 
 
@@ -30,8 +30,8 @@ def _get_proper_fixtures(fixture):
     dates = [f"{y}-{d}" for y in years for d in days]
     with open(os.path.join(HERE, 'fixtures/{}'.format(fixture))) as fh:
         x = json.load(fh)
-        # return [i for i in x.items() if i[0][5:] >= '03-01' and i[0][5:] <= '03-06']
-        return [i for i in x.items() if i[0] in dates]
+        return [i for i in x.items() if i[0][5:] >= '01-01' and i[0][5:] <= '03-11']
+        # return [i for i in x.items() if i[0] in dates]
 
 @pytest.mark.parametrize("strdate,expected_sections", _get_proper_fixtures("propers_la.json"))
 def test_all_propers_latin(strdate, expected_sections):
