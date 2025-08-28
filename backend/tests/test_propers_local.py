9d0187dba5c9a586b3dfbf1acd028eab1e94879e
mmolenda <spamm@molenda.eu>
2025-08-29 00:14:38 +0200

    local mapping for Sancti/07-26

--- backend/tests/test_propers_local.py
+++ backend/tests/test_propers_local.py
@@ -14,7 +14,7 @@ years = [
 ]
 
 days = [
-    '07-25',
+    '07-26',
     ]
 
 
@@ -115,7 +115,7 @@ def test_all_propers_english(strdate, expected_sections):
                 )
 
 
-@pytest.mark.skip
+# @pytest.mark.skip
 @pytest.mark.parametrize("language", [
     # 'la',
     'pl',
