c07f70071468f85abb48c82c8334d7c12dd2196d
mmolenda <spamm@molenda.eu>
2025-08-31 00:02:21 +0200

    update tests

--- backend/tests/test_propers_local.py
+++ backend/tests/test_propers_local.py
@@ -14,7 +14,7 @@ years = [
 ]
 
 days = [
-    '09-30',
+    '10-01',
     ]
 
 
@@ -22,6 +22,7 @@ def _get_proper_fixtures(fixture):
     dates = [f"{y}-{d}" for y in years for d in days]
     with open(os.path.join(HERE, 'fixtures/{}'.format(fixture))) as fh:
         x = json.load(fh)
+        # return [i for i in x.items() if i[0][5:] >= '01-01' and i[0][5:] <= '09-30']
         # return [i for i in x.items() if i[0][5:] >= '09-01' and i[0][5:] <= '09-20']
         return [i for i in x.items() if i[0] in dates]
 
