2e9f1b72f6b0b531fdb986dd739d807dbbc009d6
mmolenda <spamm@molenda.eu>
2025-09-02 16:31:05 +0200

    Moving sancti/12* from custom to local

--- backend/tests/test_propers_local.py
+++ backend/tests/test_propers_local.py
@@ -14,7 +14,7 @@ years = [
 ]
 
 days = [
-    '11-30',
+    '12-01',
     ]
 
 
@@ -22,7 +22,7 @@ def _get_proper_fixtures(fixture):
     dates = [f"{y}-{d}" for y in years for d in days]
     with open(os.path.join(HERE, 'fixtures/{}'.format(fixture))) as fh:
         x = json.load(fh)
-        # return [i for i in x.items() if i[0][5:] >= '01-01' and i[0][5:] <= '10-31']
+        # return [i for i in x.items() if i[0][5:] >= '01-01' and i[0][5:] <= '11-30']
         # return [i for i in x.items() if i[0][5:] >= '09-01' and i[0][5:] <= '09-20']
         return [i for i in x.items() if i[0] in dates]
 
