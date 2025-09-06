64d62c7f8695a5802a32a58c01732602b4203d15
mmolenda <spamm@molenda.eu>
2025-09-06 20:13:15 +0200

    Adjusting propers to the new structure: 05-02

--- backend/tests/test_propers_local.py
+++ backend/tests/test_propers_local.py
@@ -11,32 +11,67 @@ from .conftest import get_missal, HERE
 # 2025-04-16 W. Sroda dicuntur ommituntur etc.
 
 years = [
-    '2024',
-    '2025',
+    "2024",
+    "2025",
 ]
 
 days = [
-    "04-30"
+    "05-02",
+    # "05-03",
+    # "05-04",
+    # "05-05",
+    # "05-07",
+    # "05-08",
+    # "05-09",
+    # "05-10",
+    # "05-12",
+    # "05-14",
+    # "05-15",
+    # "05-16",
+    # "05-17",
+    # "05-18",
+    # "05-19",
+    # "05-20",
+    # "05-23",
+    # "05-24",
+    # "05-26",
+    # "05-27",
+    # "05-28",
+    # "05-29",
+    # "05-30",
+    # "05-31",
 ]
 
 
 def _get_proper_fixtures(fixture):
     dates = [f"{y}-{d}" for y in years for d in days]
-    with open(os.path.join(HERE, 'fixtures/{}'.format(fixture))) as fh:
+    with open(os.path.join(HERE, "fixtures/{}".format(fixture))) as fh:
         x = json.load(fh)
-        # return [i for i in x.items() if i[0][5:] >= '04-01' and i[0][5:] <= '04-23']
+        # return [i for i in x.items() if i[0][5:] >= "05-01" and i[0][5:] <= "05-31"]
         return [i for i in x.items() if i[0] in dates]
 
-@pytest.mark.parametrize("strdate,expected_sections", _get_proper_fixtures(f"propers_{LANGUAGE_LATIN}.json"))
+
+@pytest.mark.parametrize(
+    "strdate,expected_sections", _get_proper_fixtures(f"propers_{LANGUAGE_LATIN}.json")
+)
 def test_all_propers_latin(strdate, expected_sections):
     _tests_propers(LANGUAGE_LATIN, strdate=strdate, expected_sections=expected_sections)
 
 
-@pytest.mark.parametrize("strdate,expected_sections", _get_proper_fixtures(f"propers_{LANGUAGE_POLSKI}.json"))
+@pytest.mark.parametrize(
+    "strdate,expected_sections", _get_proper_fixtures(f"propers_{LANGUAGE_POLSKI}.json")
+)
 def test_all_propers_polish(strdate, expected_sections):
-    _tests_propers(LANGUAGE_POLSKI, strdate=strdate, expected_sections=expected_sections)
+    _tests_propers(
+        LANGUAGE_POLSKI, strdate=strdate, expected_sections=expected_sections
+    )
 
 
-@pytest.mark.parametrize("strdate,expected_sections", _get_proper_fixtures(f"propers_{LANGUAGE_ENGLISH}.json"))
+@pytest.mark.parametrize(
+    "strdate,expected_sections",
+    _get_proper_fixtures(f"propers_{LANGUAGE_ENGLISH}.json"),
+)
 def test_all_propers_english(strdate, expected_sections):
-    _tests_propers(LANGUAGE_ENGLISH, strdate=strdate, expected_sections=expected_sections)
+    _tests_propers(
+        LANGUAGE_ENGLISH, strdate=strdate, expected_sections=expected_sections
+    )
