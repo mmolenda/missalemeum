c88772c0e9ccf193003970169e8a3d042518e076
mmolenda <spamm@molenda.eu>
2025-09-08 23:38:48 +0200

    Adjusting propers to the new structure: 07-25

--- backend/tests/test_propers_local.py
+++ backend/tests/test_propers_local.py
@@ -16,8 +16,7 @@ years = [
 ]
 
 days = [
-    "07-24",
-    # "07-25",
+    "07-25",
     # "07-26",
     # "07-27",
     # "07-29",
@@ -29,8 +28,8 @@ def _get_proper_fixtures(fixture):
     dates = [f"{y}-{d}" for y in years for d in days]
     with open(os.path.join(HERE, "fixtures/{}".format(fixture))) as fh:
         x = json.load(fh)
-        return [i for i in x.items() if i[0][5:] >= "01-01" and i[0][5:] <= "07-24"]
-        # return [i for i in x.items() if i[0] in dates]
+        # return [i for i in x.items() if i[0][5:] >= "01-01" and i[0][5:] <= "07-24"]
+        return [i for i in x.items() if i[0] in dates]
 
 
 @pytest.mark.parametrize(
