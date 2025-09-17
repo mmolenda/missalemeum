7ef3fb481e96c8da8d88b764c359099b0163d240
mmolenda <spamm@molenda.eu>
2025-09-17 12:07:38 +0200

    Updating lint config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
 };
 
-module.exports = {
-  output: 'standalone'
-}
-
 export default nextConfig;
-
