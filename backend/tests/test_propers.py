45c000641dd06028d5e502ebe4271d5d496a6bd5
mmolenda <spamm@molenda.eu>
2026-05-28 12:52:35 +0200

    Fixed test

--- backend/tests/test_propers.py
+++ backend/tests/test_propers.py
@@ -107,7 +107,7 @@ def test_get_repr():
     container = missal.get_day(date(2018, 1, 13))
     assert 'Sobota po 1 Niedzieli po Objawieniu' in container.get_tempora_name()
     assert 'Wspomnienie Chrztu Pańskiego' in container.get_celebration_name()
-    assert str(container) == '[<tempora:Epi1-6:4:w>][<sancti:01-13:2:w>][]'
+    assert str(container) == '[<tempora:Epi1-6:4:w>][<sancti:01-13:2:w>][][]'
 
 
 @pytest.mark.parametrize("date_,sections", [
